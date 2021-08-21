fcef846a947bb29f9dc795c48e065c056080a5c8
Alex Harrington <spirithoula2@gmail.com>
2021-08-21 10:56:15 -0500

    connecting models to index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 const User = require('./User');
 const Login = require('./Login');
 const Calendar = require('./Calendar');
+const Student = require('./Student');
+const Teacher = require('./Teacher');
 
 module.exports = { User, Login, Calendar };
