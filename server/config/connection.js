88ab3337d4268461acc8613ac069cd32149ffbf5
Christi <mahomes4president@gmail.com>
2021-08-21 15:26:32 -0500

    fixed folder structure fixed DB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/tutor-me',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
+const connectDB = async () => {
+    try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    })
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error) {
+    console.error(`Error: ${error.message}`)
     }
-);
+    }
+// mongoose.connect(
+//     process.env.MONGODB_URI || 'mongodb://localhost/tutor-me',
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//     }
+// );
 
-module.exports = mongoose.connection;
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,29 +1,33 @@
+
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
-const db = require('./config/connection');
-
+// const db = require('./config/connection');
+const connectDB = require('./config/connection')
 const PORT = process.env.PORT || 3001;
-
+dotenv.config()
+connectDB()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
 
 // require('./routes/login_routes')(app);
 
 // app.use(require("./routes/login_routes"))(app);
 // app.use(require("./routes/recipe_routes"));
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("/*", function(req, res){
-        res.sendFile(path.join(__dirname,"./client/public/index.html"))
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+//     app.get("/*", function(req, res){
+//         res.sendFile(path.join(__dirname,"./client/public/index.html"))
+//     })
+// }
 
 
 
