c7fbfa5f4c57a85016ca8ec0632236dc661d6612
Alex Harrington <spirithoula2@gmail.com>
2021-08-21 10:30:57 -0500

    adding to server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(passport.initalize());
+app.use(cors());
+
+require('./routes/login_routes')(app);
+
+app.use(require("./routes/login_routes"))(app);
+app.use(require("./routes/recipe_routes"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function(req, res){
+        res.sendFile(path.join(__dirname,"./client/public/index.html"))
+    })
+}
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
