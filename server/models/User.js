c98b6f2492169f27670370556dec907ebb857bf1
Christi <mahomes4president@gmail.com>
2021-08-22 14:23:10 -0500

    changed database models

--- server/models/User.js
+++ server/models/User.js
@@ -1,43 +1,28 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        match: [/.+\..+/, "Must be a valid email address"],
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-    },
-    type: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: ""
-        },
-    ],
 
+const UserSchema = new mongoose.Schema({
+name: {
+type: String,
+required: true
+},
+email: {
+type: String,
+required: true,
+unique: true
+},
+password: {
+type: String,
+required: true
+},
+avatar: {
+type: String
+},
+date: {
+type: Date,
+default: Date.now
+}
 });
 
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-    next();
-});
 
-userSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
-};
-
-const User = model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('user', UserSchema);
