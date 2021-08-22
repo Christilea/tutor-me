072952ab3ebbb0a432aab5bc9d746bd0c9aa064e
Christi <mahomes4president@gmail.com>
2021-08-22 14:31:42 -0500

    added main routes for server, updated db env variables

--- server/server.js
+++ server/server.js
@@ -1,20 +1,43 @@
-
 const express = require('express');
-const app = express();
-const dotenv = require('dotenv')
-const { ApolloServer } = require('apollo-server-express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const connectDB = require('./config/db');
 const path = require('path');
 
-// const db = require('./config/connection');
-const connectDB = require('./config/connection')
-const PORT = process.env.PORT || 3001;
-dotenv.config()
-connectDB()
-app.use(express.urlencoded({ extended: true }));
+
+const app = express();
+
+
+// Connect Database
+connectDB();
+
+
+// Init Middleware
 app.use(express.json());
-app.use(cors());
+
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static('client/build'));
+
+
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
+
+
+const PORT = process.env.PORT || 5001;
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
 
 
 // require('./routes/login_routes')(app);
@@ -31,6 +54,6 @@ app.use(cors());
 
 
 
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//     console.log(`App running on port ${PORT}!`);
+// });
